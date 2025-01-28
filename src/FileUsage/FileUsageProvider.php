33f0d1d5310dce85cabdcdf76a99983566cbc687
Ingolf Steinhardt <info@e-spin.de>
2025-01-28 11:30:29 +0100

    hotfix/fix_phpcq

--- src/FileUsage/FileUsageProvider.php
+++ src/FileUsage/FileUsageProvider.php
@@ -54,6 +54,7 @@ use function urldecode;
  */
 class FileUsageProvider implements FileUsageProviderInterface
 {
+    /** @psalm-suppress MissingClassConstType */
     // phpcs:disable
     private const INSERT_TAG_PATTERN = '~{{(file|picture|figure)::([a-f0-9]{8}-[a-f0-9]{4}-1[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12})(([|?])[^}]+)?}}~';
     // phpcs:enable
